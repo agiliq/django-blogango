378ce9543b844d3cd9d3aa1d44ccad1f68514d0d
ashok.raavi <ashok.raavi@gmail.com>
2010-07-09 15:54:22 +0530

    modified pingback init statements to use pingback-0.3 version

--- blogango/__init__.py
+++ blogango/__init__.py
@@ -5,7 +5,7 @@ from time import strptime
 from django.core.urlresolvers import reverse
 from django.db.models import signals
 
-from pingback import create_ping_func
+from pingback import register_pingback, ping_func
 from pingback.client import ping_external_links, ping_directories
 from django_xmlrpc import xmlrpcdispatcher
 
@@ -18,11 +18,12 @@ def pingback_blog_handler(year, month, slug, **kwargs):
                                  is_published=True)
     
 
-ping_details = {'blogango_details': pingback_blog_handler}
+# ping_details = {'blogango_details': pingback_blog_handler}
 
-ping_func = create_ping_func(**ping_details)
+register_pingback('blogango.views.details', pingback_blog_handler)
 
 xmlrpcdispatcher.register_function(ping_func, 'pingback.ping')
 
+# ping external links in the entry
 signals.post_save.connect(ping_external_links(content_attr='text', url_attr='get_absolute_url'), sender=BlogEntry, weak=False)
 
