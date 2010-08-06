05718230b1729d8c4f4583eb0c9b01157bcc54a2
Javed Khan <javed@agiliq.com>
2010-08-06 17:48:09 +0530

    a little description

--- setup.py
+++ setup.py
@@ -6,13 +6,19 @@ setup(
     name="blogango",
     version="0.1",
     packages=['blogango'],
-    package_data={ 'blogango': [ 'templates/*.html',
-                                 'templates/blogango/*.html',
-                                 ],
-                   },
     zip_safe=False,
     author="Agiliq Solutions",
     author_email="hello@agiliq.com",
     description="Blog with django",
+    long_description=
+    """
+        Blogango is a simple but robust blogging application written with django
+        
+        Some of the features include comments using contrib.comments framework,
+        backtype and pingback support, rich text using django-markupfield,
+        month based archiving, tagging using django-tagging and categorization
+    """,
     url="http://agiliq.com/",
+    license="Dual Licenced under GPL and BSD",
+    platform="all",
 )
