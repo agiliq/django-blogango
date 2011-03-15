d6e760ed1cfb84a2d30de168963a9bfc93dc29c0
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-15 15:34:39 +0530

    Prevent pages from coming into feeds.

--- blogango/feeds.py
+++ blogango/feeds.py
@@ -20,7 +20,7 @@ class main_feed(Feed):
     description = blog.tag_line
     
     def items (self):
-        return BlogEntry.objects.filter(is_published=True)[:10]
+        return BlogEntry.objects.filter(is_published=True, is_page=False)[:10]
 
     def item_description(self, item):
         return item.text
@@ -36,11 +36,11 @@ class CatFeed(Feed):
     
     def link(self, obj):
         if not obj:
-            raise FeedDoesNotExist
+            raise ObjectDoesNotExist
         return reverse('blogango_tag_details', args=[obj.slug])
     
     def description(self, obj):
         return "Category: %s" % obj.name
     
     def items(self, obj):
-        return BlogEntry.objects.filter(is_published=True, tags__in=[obj])
+        return BlogEntry.objects.filter(is_published=True, tags__in=[obj], is_page=False)
