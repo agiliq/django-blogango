718fbefd8912033a5cafbb1abd9638f45d8d1c5b
Plaban <plaban.nayak@gmail.com>
2011-09-22 14:34:29 +0530

    first successful test

--- blogango/tests.py
+++ blogango/tests.py
@@ -3,6 +3,8 @@ from models import Blog
 
 class BlogTestCase(unittest.TestCase):
     def setUp(self):
-        self.Blog = Blog(title = "test",tag_line = "new blog",entries_per_page=10,recents = 5, recent_comments = 5)
+        self.blog = Blog(title = "test",tag_line = "new blog",entries_per_page=10,recents = 5, recent_comments = 5)
+        self.blog.save()
     def test_single_existence(self):
-        pass
+        blog = Blog(title = "test",tag_line = "new blog",entries_per_page=10,recents = 5, recent_comments = 5)
+        self.assertRaises(Exception,blog.save())
