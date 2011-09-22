51c49c50a57543807f1d54468cf783002d079ae9
Plaban <plaban.nayak@gmail.com>
2011-09-22 18:33:05 +0530

    added tests for testing posting of entry

--- blogango/tests.py
+++ blogango/tests.py
@@ -1,7 +1,8 @@
 from django.utils import unittest
-from models import Blog
+from models import Blog,BlogEntry
 from django.test.client import Client
-
+from datetime import datetime
+from django.contrib.auth.models import User
 class BlogTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -14,12 +15,39 @@ class BlogTestCase(unittest.TestCase):
         self.assertRaises(Exception,blog.save())
 
 
-class TestIndexView(unittest.TestCase):
-    """Test the first page of the blog"""
+class TestViews(unittest.TestCase):
+    """Test pages  of the blog"""
 
     def test_first_page(self):
         c = Client()
         response = c.get("/blog/")
+        #it should return 200 for all users
         self.assertEqual(response.status_code,200)
+        
+    def test_admin_page(self):
+        c = Client()
+        response = c.get("/blog/admin/")
+        self.assertEqual(response.status_code , 200)
+
+    def test_add_entry(self):
+        c = Client()
+        user  = User.objects.create_user('gonecrazy','gonecrazy@gmail.com','gonecrazy')
+        user.is_staff = True
+        user.save()
+        response = c.login(username='gonecrazy',password='gonecrazy')
+        response = c.post("/blog/admin/entry/new/",{'title':'my new blog post','text':'this is the test post','publish_date_0':'2011-09-22','publish_date_1':'17:17:55','text_markup_type':"html",'created_by':1})
+        #check for successful posting of entry
+        self.assertEqual(response.status_code,302)
+        blog = BlogEntry.default.all()
+        self.assertEqual(1,blog.count())
+        
+
+        
+
+        
+
+        
+        
+        
     
 
