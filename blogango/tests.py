f28ab32f86875695bc23dd455b7c08267c310e2c
Plaban <plaban.nayak@gmail.com>
2011-09-22 19:19:19 +0530

    added test to check for existence of post

--- blogango/tests.py
+++ blogango/tests.py
@@ -17,29 +17,35 @@ class BlogTestCase(unittest.TestCase):
 
 class TestViews(unittest.TestCase):
     """Test pages  of the blog"""
+    def setUp(self):
+        self.c = Client()
+        
 
     def test_first_page(self):
-        c = Client()
-        response = c.get("/blog/")
+        response = self.c.get("/blog/")
         #it should return 200 for all users
         self.assertEqual(response.status_code,200)
         
     def test_admin_page(self):
-        c = Client()
-        response = c.get("/blog/admin/")
+        response = self.c.get("/blog/admin/")
         self.assertEqual(response.status_code , 200)
 
     def test_add_entry(self):
-        c = Client()
-        user  = User.objects.create_user('gonecrazy','gonecrazy@gmail.com','gonecrazy')
+        user  = User.objects.create_user(username = 'gonecrazy',email='gonecrazy@gmail.com',password = 'gonecrazy')
         user.is_staff = True
         user.save()
-        response = c.login(username='gonecrazy',password='gonecrazy')
-        response = c.post("/blog/admin/entry/new/",{'title':'my new blog post','text':'this is the test post','publish_date_0':'2011-09-22','publish_date_1':'17:17:55','text_markup_type':"html",'created_by':1})
+        response = self.c.login(username='gonecrazy',password='gonecrazy')
+        response = self.c.post("/blog/admin/entry/new/",{'title':'test post','text':'this is the test post','publish_date_0':'2011-09-22','publish_date_1':'17:17:55','text_markup_type':"html",'created_by':1,'publish':'Save and Publish'})
         #check for successful posting of entry
         self.assertEqual(response.status_code,302)
         blog = BlogEntry.default.all()
         self.assertEqual(1,blog.count())
+    
+    
+    def test_entry_display(self):
+        response = self.c.get('/blog/9999/12/test-post/')
+        #check if the added post can be retreived 
+        self.assertEqual(response.status_code,200)
         
 
         
