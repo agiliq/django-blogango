e8fe48cef8b9f985c5d8d4468237fb94ba549d3e
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-10-14 15:13:25 +0530

    Bumped the version once more in the day.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django-blogango",
-    version="0.2",
+    version="0.3",
     packages=['blogango',
               'blogango/conf',
               'blogango/management',
