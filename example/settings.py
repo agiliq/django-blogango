8188f2a18872b2d05c8bc9e966a73e3bac385021
Shabda Raaj <shabda@agiliq.com>
2012-10-08 18:41:02 +0530

    Removed duplicate entry for auth.

--- example/settings.py
+++ example/settings.py
@@ -70,7 +70,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.auth',
     'django.contrib.admin',
     'django.contrib.markup',
     'django.contrib.humanize',
