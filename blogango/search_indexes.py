8c1fc22ad6a08aec35bbbaa5365e2a6f4be93f73
Ismael Mendonca <irmt06@gmail.com>
2014-01-20 16:09:21 -0430

    Added "using", for haystack compatibility.

--- blogango/search_indexes.py
+++ blogango/search_indexes.py
@@ -8,16 +8,16 @@ class BlogEntryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(model_attr='text', document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     summary = indexes.CharField(model_attr='summary')
-    created_by = indexes.CharField(model_attr='created_by') 
-    
-    meta_keywords = indexes.CharField(model_attr='meta_keywords') 
+    created_by = indexes.CharField(model_attr='created_by')
+
+    meta_keywords = indexes.CharField(model_attr='meta_keywords')
     meta_description = indexes.CharField(model_attr='meta_description')
-    
+
     tags = indexes.CharField(model_attr='tags')
 
     def get_model(self):
         return BlogEntry
 
-    def index_queryset(self):
+    def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(publish_date__lte=datetime.datetime.now())
