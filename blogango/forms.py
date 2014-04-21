3356697bae4fdc13f75692832b785b83bb5aa733
krvc <krvc.it@gmail.com>
2014-04-21 17:54:36 +0530

    Pep8 fixes

--- blogango/forms.py
+++ blogango/forms.py
@@ -4,10 +4,12 @@ from django.contrib.admin import widgets
 
 from blogango.models import Blog, BlogRoll, BlogEntry
 
+
 class WideTextArea(forms.Textarea):
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('attrs',{}).update({'rows': '20', 'cols':'80'})
+        kwargs.setdefault('attrs', {}).update({'rows': '20', 'cols': '80'})
         super(WideTextArea, self).__init__(*args, **kwargs)
+
 
 class EntryForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -15,18 +17,27 @@ class EntryForm(forms.ModelForm):
         self.fields['created_by'].queryset = User.objects.filter(is_staff=True)
         self.fields['publish_date'].widget = widgets.AdminSplitDateTime()
 
-    title = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'size':'40'}))
+    title = forms.CharField(max_length=100,
+                            required=False,
+                            widget=forms.TextInput(attrs={'size': '40'}))
     text = forms.CharField(widget=WideTextArea(attrs={'class': 'resizable'}))
 
     class Meta:
         model = BlogEntry
         exclude = ('slug', 'summary')
 
+
 class CommentForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'textarea'}), label='Comment')
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'textfield'}))
-    url = forms.URLField(required=False, widget=forms.TextInput(attrs={'class': 'textfield'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'textfield'}))
+    text = forms.CharField(widget=forms.Textarea(attrs={'class': 'textarea'}),
+                           label='Comment')
+    name = forms.CharField(max_length=100,
+                           widget=forms.TextInput(attrs={'class':
+                                                         'textfield'}))
+    url = forms.URLField(required=False,
+                         widget=forms.TextInput(attrs={'class': 'textfield'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class':
+                                                           'textfield'}))
+
 
 class InstallForm(forms.ModelForm):
 
@@ -34,10 +45,12 @@ class InstallForm(forms.ModelForm):
         model = Blog
         exclude = ('entries_per_page', 'recents', 'recent_comments')
 
+
 class PreferencesForm(forms.ModelForm):
 
     class Meta:
         model = Blog
+
 
 class BlogForm(forms.ModelForm):
 
