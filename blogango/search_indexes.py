5af3577a8090f3a17bea89f08550a9e7a66d8ff5
krvc <krvc.it@gmail.com>
2014-04-21 18:13:21 +0530

    Pep8 Fixes

--- blogango/search_indexes.py
+++ blogango/search_indexes.py
@@ -5,14 +5,15 @@ from blogango.models import BlogEntry
 
 
 class BlogEntryIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(model_attr='text', document=True, use_template=True)
+    text = indexes.CharField(model_attr='text',
+                             document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     summary = indexes.CharField(model_attr='summary')
-    created_by = indexes.CharField(model_attr='created_by') 
-    
-    meta_keywords = indexes.CharField(model_attr='meta_keywords') 
+    created_by = indexes.CharField(model_attr='created_by')
+
+    meta_keywords = indexes.CharField(model_attr='meta_keywords')
     meta_description = indexes.CharField(model_attr='meta_description')
-    
+
     tags = indexes.CharField(model_attr='tags')
 
     def get_model(self):
@@ -20,4 +21,5 @@ class BlogEntryIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(publish_date__lte=datetime.datetime.now())
+        return self.get_model().objects.filter(
+            publish_date__lte=datetime.datetime.now())
