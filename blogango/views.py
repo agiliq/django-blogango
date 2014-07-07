7fef03e01aa8412dad125c24fb40787e780e8e2e
omkarvijay5 <omkarvijay5@gmail.com>
2014-07-07 12:34:02 +0530

    By checking pyflakes removed datetime and paginator as they were unused from views.py

--- blogango/views.py
+++ blogango/views.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.shortcuts import render_to_response, get_object_or_404, redirect, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.conf import settings
@@ -6,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.template import RequestContext
-from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.core.exceptions import ObjectDoesNotExist
